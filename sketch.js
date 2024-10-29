6072cbcf026a3aa963af5f1531bfbacf9523f008
Beta2learn <176282205+Beta2learn@users.noreply.github.com>
2024-10-29 08:29:21 +0100

    added select options for different shapes

--- sketch.js
+++ sketch.js
@@ -1,8 +1,41 @@
+//Project title
 const title = document.createElement("h1");
 title.textContent = "Etch-a-Sketch";
 title.classList.add("title");
 document.body.appendChild(title);
 
+//Container
 const container = document.createElement("div");
 container.id("container");
 document.body.appendChild(container);
+
+//Clear content in the container
+function clearContent(){
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+}
+
+const menuItems = document.createElement("div");
+
+//Drop-down menu for grid shape selection
+const gridShape = document.createElement("select");
+gridShape.classList.add("gridshape");
+
+//Shape options
+const shapeOne = document.createElement("option");
+shapeOne.classList.add("shapeone");
+
+const shapeTwo = document.createElement("option");
+shapeTwo.classList.add("shapetwo");
+
+const shapeThree = document.createElement("option");
+shapeThree.classList.add("shapethree");
+
+menuItems.appendChild(gridShape);
+
+gridShape.appendChild(shapeOne);
+gridShape.appendChild(shapeTwo);
+gridShape.appendChild(shapeThree);
+
+document.body.insertBefore(menuItems, container);
